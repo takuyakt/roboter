466947e47a70c8c59780051b9c3d09052f0a5fba
takuyakt <10061798+takuyakt@users.noreply.github.com>
2020-05-06 17:36:25 +0900

    WIP:implement function of write_csv_ranking.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import tempfile
 
 import termcolor
 
@@ -7,7 +8,7 @@ welcome_text = """\
 こんにちは!私はRobokoです。あなたの名前は何ですか?
 """
 
-question_text="""\
+question_text = """\
 {0}さん。どこのレストランが好きですか?
 """
 
@@ -21,10 +22,28 @@ def green_print(text: str):
     print(termcolor.colored(text, color='green'))
 
 
-def write_csv_ranking(file_name:str):
-     if not os.path.isfile(file_name):
-        with open(file_name, 'w') as f:
-            csv.DictWriter() #Todo csvに書き込む
+def write_csv_ranking(csv_file_name: str, restaurant_name: str):
+    header = ["NAME", "COUNT"]
+
+#    if not os.path.isfile(csv_file_name):
+#        with open(csv_file_name, 'w', newline="") as template_file:
+#            writer = csv.DictWriter(template_file, fieldnames=header)
+#            writer.writeheader()
+
+    try:
+        with open(csv_file_name, 'r+', newline="") as csv_file:
+            reader = csv.DictReader(csv_file, fieldnames=header)
+    except:
+        pass
+
+    with tempfile.NamedTemporaryFile(delete=False) as new_file:
+        writer = csv.DictWriter(new_file, fieldnames=header)
+        writer.writeheader()
+        for row in reader:
+            if row["NAME"] == restaurant_name:
+                pass
+#            writer.writerow({"NAME": restaurant_name, "COUNT": 1})  # Todo カウントアップする。
+
 
 def main():
     while True:
@@ -35,9 +54,11 @@ def main():
 
     while True:
         green_print(question_text.format(user_name))
-        user_favorite_restaurant = input()
-        if len(user_favorite_restaurant) != 0:
+        favorite_restaurant_name = input()
+        if len(favorite_restaurant_name) != 0:
             break
+
+    write_csv_ranking('ranking.csv', favorite_restaurant_name)
     green_print(goodbye_text.format(user_name))
 
 
